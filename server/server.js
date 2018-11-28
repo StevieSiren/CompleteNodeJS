4679ea1e682c0e87b223ba7b76c37bbaffd84790
Stephen Thomas <stevie.t.siren@gmail.com>
2018-11-27 20:42:49 -0800

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Library imports
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    {ObjectID} = require('mongodb');
 
 
 // Local imports
@@ -35,11 +36,33 @@ app.get('/todos', (req, res) => {
       }, (err) => {
         res.status(400).send(err);
       });
-})
+});
+
+
+// GET /todos/:id
+// Test ID: 5bfcb588daf66b702b2cdce9
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    // Find by ID
+    Todo.findById(id)
+      .then((todo) => {
+          if(!todo) {
+              return res.status(400).send();
+          }
+          res.send({todo});
+      })
+      .catch((err) => {
+          res.status(400).send();
+      });
+});
 
 
 
-
+// =======================================================================
 app.listen(3000, () => {
     console.log('Constructing additional pylons...');
 });
